fda7024fbee804a7e7c7a069ca861a73fd461e01
Ratz Nikita <ratsnikita163@gmail.com>
2023-08-09 11:34:12 +0400

    add Login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,26 @@
+import './Login.css'
+import {Input} from "../Input/Input";
+import React from 'react'
+import {AuthForm} from "../Form/AuthForm";
+
+
+export const Login = () => {
+
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const onFormSubmit = (e) => {
+        console.log(e.target.elements)
+    }
+
+
+    return (
+        <section className='signin'>
+            <AuthForm type='signin' onSubmit={onFormSubmit} noValidate>
+                <Input type='email' name='E-mail' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <Input minLength={2} maxLength={30} type='password' name='Пароль' value={password}
+                       onChange={(e) => setPassword(e.target.value)}/>
+            </AuthForm>
+        </section>
+    )
+}
