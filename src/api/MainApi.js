8efd74b193ad52445201d76c2edea378bd05044d
Ratz Nikita <ratsnikita163@gmail.com>
2023-08-13 08:59:39 +0400

    add api

---
+++ src/api/MainApi.js
@@ -0,0 +1,74 @@
+class Api {
+    constructor(options) {
+        this._options = options
+    }
+
+    signUp(userInfo) {
+        return fetch(`${this._options.baseUrl}/signup`, {
+            method: 'POST', headers: this._options.headers,
+            body: JSON.stringify(userInfo)
+        })
+            .then(res => {
+                return this._checkStatus(res)
+            })
+    }
+
+    signIn(userInfo) {
+        return fetch(`${this._options.baseUrl}/signin`, {
+            method: 'POST', headers: this._options.headers,
+            body: JSON.stringify(userInfo)
+        })
+            .then(res => {
+                return this._checkStatus(res)
+            }).then(token => this.setToken(token))
+    }
+
+    getUserInfo() {
+        return fetch(`${this._options.baseUrl}/users/me`, {
+            method: 'GET', headers: this._options.headers,
+        })
+            .then(res => {
+                return this._checkStatus(res)
+            })
+    }
+
+    postMovies(movie) {
+        return fetch(`${this._options.baseUrl}/movies`, {
+            method: 'POST', headers: this._options.headers,
+            body: JSON.stringify(...movie)
+        })
+    }
+
+    removeMovie(id) {
+        return fetch(`${this._options.baseUrl}/cards/${id}`, {
+            method: 'DELETE', headers: this._options.headers,
+        })
+            .then(res => {
+                return this._checkStatus(res)
+            })
+    }
+
+    setUserInfo(name, email) {
+        return fetch(`${this._options.baseUrl}/users/me`, {
+            method: 'PATCH', headers: this._options.headers, body: JSON.stringify(name,email)
+        })
+            .then(res => {
+                return this._checkStatus(res)
+            })
+    }
+
+
+    _checkStatus(res) {
+        return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
+    }
+
+    setToken(token) {
+        this._options.headers.authorization = `Bearer ${token}`
+    }
+}
+
+export const api = new Api({
+    baseUrl: 'https://api.ratz-movies-search.nomoreparties.co', headers: {
+        authorization: 'Bearer ' + localStorage.getItem('token'), 'Content-Type': 'application/json'
+    }
+});
---
+++ src/api/MoviesApi.js
@@ -0,0 +1,24 @@
+class Api {
+    constructor(options) {
+        this._options = options
+    }
+
+    getMovies() {
+        return fetch(`${this._options.baseUrl}`, {
+            method: 'GET', headers: this._options.headers
+        })
+            .then(res => {
+                return this._checkStatus(res)
+            })
+    }
+
+    _checkStatus(res) {
+        return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
+    }
+}
+
+export const moviesApi = new Api({
+    baseUrl: 'https://api.nomoreparties.co/beatfilm-movies', headers: {
+        'Content-Type': 'application/json'
+    }
+});
